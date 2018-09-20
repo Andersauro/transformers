4a08f7061d19bde1eb56ace2b7c88834b606342e
Anderson Meira <andersonfarias8@gmail.com>
2018-09-20 10:51:25 -0700

    Add docs URL
    Add swagger docs url

--- src/main/java/ca/eaq/transformers/models/UrlInfo.java
+++ src/main/java/ca/eaq/transformers/models/UrlInfo.java
@@ -5,11 +5,13 @@ public class UrlInfo {
 	private String transformers;
 	private String transformer;
 	private String battle;
+	private String documentation;
 
 	public UrlInfo() {
 		this.transformers = "/transformers/";
 		this.transformer = "/transformer/{id}/";
 		this.battle = "/battle/";
+		this.documentation = "/swagger-ui.html#/";
 	}
 
 	public String getTransformers() {
@@ -22,6 +24,10 @@ public class UrlInfo {
 
 	public String getBattle() {
 		return battle;
+	}
+	
+	public String getDocumentation() {
+		return documentation;
 	}
 
 }
